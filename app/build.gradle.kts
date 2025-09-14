7d2511ff13aaef0cb1aac885be6ab182c0332a42
Bhagyesh Patil <bhagyeshp013@gmail.com>
2025-09-14 17:48:38 +0530

    chore(deps): add Navigation Compose, Material Icons Extended, Datastore, Security Crypto and update existing libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.security.crypto)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
